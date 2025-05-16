153c8316697a7941398dc4f778679476b1990add
Yaroslav Rezov <sisposis1@gmail.com>
2025-05-16 19:31:27 +0300

    check

--- src/main/java/com/example/computershop/controllerCustomer/LaptopCustomerController.java
+++ src/main/java/com/example/computershop/controllerCustomer/LaptopCustomerController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/customer/laptops")
 public class LaptopCustomerController {
+
     private final LaptopService laptopService;
 
     @GetMapping("/all")
