18882889b4d43b4250e537c73d1f8327f2c5032c
Yaroslav Rezov <sisposis1@gmail.com>
2025-05-18 21:46:23 +0300

    now laptops and printers and pcs can do the same as in index.

--- src/main/java/com/example/computershop/controllerCustomer/LaptopCustomerController.java
+++ src/main/java/com/example/computershop/controllerCustomer/LaptopCustomerController.java
@@ -1,6 +1,8 @@
 package com.example.computershop.controllerCustomer;
 
+import com.example.computershop.model.dto.CartDto;
 import com.example.computershop.model.dto.LaptopDto;
+import com.example.computershop.service.CartService;
 import com.example.computershop.service.LaptopService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.List;
 public class LaptopCustomerController {
 
     private final LaptopService laptopService;
+    private final CartService cartService;
 
     @GetMapping("/all")
     List<LaptopDto> getLaptops(){
@@ -22,6 +25,12 @@ public class LaptopCustomerController {
     LaptopDto getLaptop(@RequestParam Long code){
         return laptopService.getLaptop(code);
     }
+
+    @PostMapping()
+    CartDto insertIntoCart(@RequestBody CartDto cartDto) {
+        return cartService.save(cartDto);
+    }
+
 
 }
 
--- src/main/java/com/example/computershop/controllerCustomer/PrinterCustomerController.java
+++ src/main/java/com/example/computershop/controllerCustomer/PrinterCustomerController.java
@@ -1,5 +1,7 @@
 package com.example.computershop.controllerCustomer;
+import com.example.computershop.model.dto.CartDto;
 import com.example.computershop.model.dto.PrinterDto;
+import com.example.computershop.service.CartService;
 import com.example.computershop.service.PrinterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import java.util.List;
 @RequestMapping("/customer/printers")
 public class PrinterCustomerController {
     private final PrinterService printerService;
+    private final CartService cartService;
 
     @GetMapping("/all")
     List<PrinterDto> getPrinters(){
@@ -19,5 +22,10 @@ public class PrinterCustomerController {
     @GetMapping
     PrinterDto getPrinter(@RequestParam Long code){
         return printerService.getPrinter(code);
+    }
+
+    @PostMapping()
+    CartDto insertIntoCart(@RequestBody CartDto cartDto) {
+        return cartService.save(cartDto);
     }
 }
