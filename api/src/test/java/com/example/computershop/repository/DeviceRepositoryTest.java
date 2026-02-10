e020e41e2259b713534d45915abfd9a3c9ab8260
Yaroslav Rezov <sisposis1@gmail.com>
2026-02-10 22:46:44 +0300

    repoTests(#51, #52, #53): copied a better way to equalize

--- api/src/test/java/com/example/computershop/repository/DeviceRepositoryTest.java
+++ api/src/test/java/com/example/computershop/repository/DeviceRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.computershop.repository;
 import com.example.computershop.model.entity.DeviceView;
 import com.example.computershop.model.entity.PcEntity;
 import com.example.computershop.model.entity.ProductEntity;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -28,20 +29,23 @@ public class DeviceRepositoryTest extends BaseIT {
     @Autowired
     private PcRepository pcRepository;
 
+    private ProductEntity productEntity;
+    private PcEntity pcEntity;
+
+    @BeforeEach
+    void setUp() {
+        productEntity = createProductEntity1();
+        pcEntity = createPcEntity1();
+    }
+
     @Test
     void findDeviceByCode() {
-        ProductEntity productEntity = createProductEntity1();
-
         ProductEntity savedProductEntity = productRepository.save(productEntity);
-
-        PcEntity pcEntity = createPcEntity1();
         pcEntity.setProduct(savedProductEntity);
-
         PcEntity savedPcEntity = pcRepository.save(pcEntity);
+        DeviceView expected = createDeviceViewMock1(savedPcEntity.getProduct().getModel());
 
         DeviceView actual = deviceRepository.findDeviceByCode(savedPcEntity.getProduct().getModel());
-
-        DeviceView expected = createDeviceViewMock1(savedPcEntity.getProduct().getModel());
 
         assertEquals(expected.getModel(), actual.getModel());
         assertEquals(expected.getPrice(), actual.getPrice());
@@ -49,32 +53,21 @@ public class DeviceRepositoryTest extends BaseIT {
 
     @Test
     void findAllDevices() {
-        ProductEntity productEntity = createProductEntity1();
-
         ProductEntity savedProductEntity = productRepository.save(productEntity);
-
-        PcEntity pcEntity = createPcEntity1();
         pcEntity.setProduct(savedProductEntity);
-
         PcEntity savedPcEntity = pcRepository.save(pcEntity);
-
-
         ProductEntity productEntity2 = createProductEntity2();
-
         ProductEntity savedProductEntity2 = productRepository.save(productEntity2);
-
         PcEntity pcEntity2 = createPcEntity2();
         pcEntity2.setProduct(savedProductEntity2);
-
         PcEntity savedPcEntity2 = pcRepository.save(pcEntity2);
+        List<DeviceView> actual = deviceRepository.findAllDevices();
 
-        List<DeviceView> expected = deviceRepository.findAllDevices();
+        List<DeviceView> expected = new ArrayList<>();
+        expected.add(createDeviceViewMock1(savedPcEntity.getProduct().getModel()));
+        expected.add(createDeviceViewMock2(savedPcEntity2.getProduct().getModel()));
 
-        List<DeviceView> actual = new ArrayList<>();
-        actual.add(createDeviceViewMock1(savedPcEntity.getProduct().getModel()));
-        actual.add(createDeviceViewMock2(savedPcEntity2.getProduct().getModel()));
-
-        for(int i = 0; i < actual.size(); i++){
+        for(int i = 0; i < expected.size(); i++){
         assertEquals(expected.get(i).getModel(), actual.get(i).getModel());
         assertEquals(expected.get(i).getPrice(), actual.get(i).getPrice());
         }
--- api/src/test/java/com/example/computershop/repository/ProductRepositoryTest.java
+++ api/src/test/java/com/example/computershop/repository/ProductRepositoryTest.java
@@ -12,8 +12,7 @@ import static com.example.computershop.data.PcEntityData.createPcEntity1;
 import static com.example.computershop.data.PcEntityData.createPcEntity2;
 import static com.example.computershop.data.ProductEntityData.createProductEntity1;
 import static com.example.computershop.data.ProductEntityData.createProductEntity2;
-import static com.example.computershop.data.ProductJoinedViewData.createProductJoinedViewMock1;
-import static com.example.computershop.data.ProductJoinedViewData.createProductJoinedViewMock2;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProductRepositoryTest extends BaseIT{
@@ -23,34 +22,31 @@ public class ProductRepositoryTest extends BaseIT{
     private PcRepository pcRepository;
     @Test
     void findAllProductsJoined() {
+        ProductEntity product1 = productRepository.save(createProductEntity1());
+        PcEntity pc1 = createPcEntity1();
+        pc1.setProduct(product1);
+        PcEntity expected1 = pcRepository.save(pc1);
 
-        ProductEntity productEntity = createProductEntity1();
+        ProductEntity product2 = productRepository.save(createProductEntity2());
+        PcEntity pc2 = createPcEntity2();
+        pc2.setProduct(product2);
+        PcEntity expected2 = pcRepository.save(pc2);
 
-        ProductEntity savedProductEntity = productRepository.save(productEntity);
+        List<ProductJoinedView> actual = productRepository.findAllProductsJoined();
 
-        PcEntity pcEntity = createPcEntity1();
-        pcEntity.setProduct(savedProductEntity);
-
-        PcEntity savedPcEntity = pcRepository.save(pcEntity);
-
-
-        ProductEntity productEntity2 = createProductEntity2();
-
-        ProductEntity savedProductEntity2 = productRepository.save(productEntity2);
-
-        PcEntity pcEntity2 = createPcEntity2();
-        pcEntity2.setProduct(savedProductEntity2);
-
-        PcEntity savedPcEntity2 = pcRepository.save(pcEntity2);
-
-        List<ProductJoinedView> actual = List.of(createProductJoinedViewMock1(savedPcEntity.getProduct().getModel(), savedPcEntity.getCode()),
-                createProductJoinedViewMock2(savedPcEntity2.getProduct().getModel(), savedPcEntity2.getCode()));
-        List<ProductJoinedView> expected = productRepository.findAllProductsJoined();
-        for(int i = 0; i < actual.size(); i++){
-            assertEquals(expected.get(i).getModel(), actual.get(i).getModel());
-            assertEquals(expected.get(i).getMaker(), actual.get(i).getMaker());
-            assertEquals(expected.get(i).getType(), actual.get(i).getType());
-            assertEquals(expected.get(i).getCode(), actual.get(i).getCode());
-        }
+        assertThat(actual)
+                .hasSize(2)
+                .anySatisfy(actual1 -> {
+                    assertEquals(expected1.getProduct().getModel(), actual1.getModel());
+                    assertEquals(expected1.getProduct().getMaker(), actual1.getMaker());
+                    assertEquals(expected1.getProduct().getType(), actual1.getType());
+                    assertEquals(expected1.getCode(), actual1.getCode());
+                })
+                .anySatisfy(actual2 -> {
+                    assertEquals(expected2.getProduct().getModel(), actual2.getModel());
+                    assertEquals(expected2.getProduct().getMaker(), actual2.getMaker());
+                    assertEquals(expected2.getProduct().getType(), actual2.getType());
+                    assertEquals(expected2.getCode(), actual2.getCode());
+                });
     }
 }
