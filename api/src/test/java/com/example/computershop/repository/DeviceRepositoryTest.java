8fac72071325dfe89890c01834cdaa2ebd3b75fd
Yaroslav Rezov <sisposis1@gmail.com>
2026-02-13 23:34:12 +0300

    repoTests(#51, #52, #53): once again copied

--- api/src/test/java/com/example/computershop/repository/DeviceRepositoryTest.java
+++ api/src/test/java/com/example/computershop/repository/DeviceRepositoryTest.java
@@ -16,6 +16,7 @@ import static com.example.computershop.data.PcEntityData.createPcEntity1;
 import static com.example.computershop.data.PcEntityData.createPcEntity2;
 import static com.example.computershop.data.ProductEntityData.createProductEntity1;
 import static com.example.computershop.data.ProductEntityData.createProductEntity2;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeviceRepositoryTest extends BaseIT {
@@ -34,43 +35,40 @@ public class DeviceRepositoryTest extends BaseIT {
 
     @BeforeEach
     void setUp() {
-        productEntity = createProductEntity1();
+        productEntity = productRepository.save(createProductEntity1());
         pcEntity = createPcEntity1();
+        pcEntity.setProduct(productEntity);
+        pcEntity = pcRepository.save(pcEntity);
     }
 
     @Test
     void findDeviceByCode() {
-        ProductEntity savedProductEntity = productRepository.save(productEntity);
-        pcEntity.setProduct(savedProductEntity);
-        PcEntity savedPcEntity = pcRepository.save(pcEntity);
-        DeviceView expected = createDeviceViewMock1(savedPcEntity.getProduct().getModel());
+        DeviceView expected = createDeviceViewMock1(pcEntity.getProduct().getModel());
 
-        DeviceView actual = deviceRepository.findDeviceByCode(savedPcEntity.getProduct().getModel());
+        DeviceView actual = deviceRepository.findDeviceByCode(pcEntity.getProduct().getModel());
 
-        assertEquals(expected.getModel(), actual.getModel());
-        assertEquals(expected.getPrice(), actual.getPrice());
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
     }
 
     @Test
     void findAllDevices() {
-        ProductEntity savedProductEntity = productRepository.save(productEntity);
-        pcEntity.setProduct(savedProductEntity);
-        PcEntity savedPcEntity = pcRepository.save(pcEntity);
         ProductEntity productEntity2 = createProductEntity2();
         ProductEntity savedProductEntity2 = productRepository.save(productEntity2);
         PcEntity pcEntity2 = createPcEntity2();
         pcEntity2.setProduct(savedProductEntity2);
         PcEntity savedPcEntity2 = pcRepository.save(pcEntity2);
-        List<DeviceView> actual = deviceRepository.findAllDevices();
-
         List<DeviceView> expected = new ArrayList<>();
-        expected.add(createDeviceViewMock1(savedPcEntity.getProduct().getModel()));
+
+        List<DeviceView> actual = deviceRepository.findAllDevices();
+        expected.add(createDeviceViewMock1(pcEntity.getProduct().getModel()));
         expected.add(createDeviceViewMock2(savedPcEntity2.getProduct().getModel()));
 
-        for(int i = 0; i < expected.size(); i++){
-        assertEquals(expected.get(i).getModel(), actual.get(i).getModel());
-        assertEquals(expected.get(i).getPrice(), actual.get(i).getPrice());
-        }
+        assertThat(actual)
+                .hasSize(2)
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
     }
 
 }
